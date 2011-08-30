5f6d4aa6fcf9ddcdd2af39ca4b2e0a5d19cfb904
Ben <ben@benknight.me>
2011-08-30 17:29:22 -0400

    Unbranding master branch (will create 3c32-branded branch)

--- functions.php
+++ functions.php
@@ -12,13 +12,11 @@ function twentyeleven_setup() { return false; }
 function twentyeleven_get_theme_options() { return false; }
 function disable_stuff() { return false; }
 
-add_action( 'after_setup_theme', 'custom_theme_setup' );
-function custom_theme_setup() {
+add_action( 'after_setup_theme', 'miseenplace_theme_setup' );
+function miseenplace_theme_setup() {
 
 	// plugins
-	require 'plugins/breadcrumb-trail/breadcrumb-trail.php';
-	require 'plugins/section-subnav.php';
-	require 'plugins/posts-widget.php';
+	require 'plugins/section-subnav/section-subnav.php';
 
 	// editor css
 	add_editor_style();
@@ -50,17 +48,17 @@ function custom_theme_setup() {
 /**
  * wp_page_menu() args
  */
-function custom_page_menu_args( $args ) {
+function miseenplace_page_menu_args( $args ) {
 	$args['show_home'] = false;
 	return $args;
 }
-add_filter( 'wp_page_menu_args', 'custom_page_menu_args' );
+add_filter( 'wp_page_menu_args', 'miseenplace_page_menu_args' );
 
 /**
  * Default Dashboard settings
  */
-add_action( 'user_register', 'custom_dashboard_widgets' );
-function custom_dashboard_widgets( $user_id ) {
+add_action( 'user_register', 'miseenplace_dashboard_widgets' );
+function miseenplace_dashboard_widgets( $user_id ) {
 	update_user_meta( $user_id, 'metaboxhidden_dashboard', array(
 		'dashboard_recent_comments',
 		'dashboard_plugins',
@@ -70,13 +68,13 @@ function custom_dashboard_widgets( $user_id ) {
 		'dashboard_incoming_links',
 		'dashboard_quick_press'
 	));
-	add_option( 'custom_dashboard', 1 );
+	add_option( 'miseenplace_dashboard', 1 );
 	// TODO: add page speed widget
 }
-if ( is_admin() && ! get_option( 'custom_dashboard' ) ) {
+if ( is_admin() && ! get_option( 'miseenplace_dashboard' ) ) {
 	global $current_user;
 	get_currentuserinfo();
-	custom_dashboard_widgets( $current_user->ID );
+	miseenplace_dashboard_widgets( $current_user->ID );
 }
 
 /*
@@ -84,8 +82,8 @@ if ( is_admin() && ! get_option( 'custom_dashboard' ) ) {
  * ( scripts loaded in footer for faster page loading )
  *
  */
-add_action( 'init', 'custom_scripts_and_styles' );
-function custom_scripts_and_styles() {
+add_action( 'init', 'miseenplace_scripts_and_styles' );
+function miseenplace_scripts_and_styles() {
 
 	if ( ! is_admin() ) {
 	
@@ -159,8 +157,8 @@ function custom_scripts_and_styles() {
  * Register our sidebars and widgetized areas. Also register the default Epherma widget.
  *
  */
-add_action( 'widgets_init', 'custom_widgets_init' );
-function custom_widgets_init() {
+add_action( 'widgets_init', 'miseenplace_widgets_init' );
+function miseenplace_widgets_init() {
 
 	register_sidebar( array(
 		'name' => 'Default Sidebar',
@@ -193,8 +191,8 @@ function custom_widgets_init() {
 /**
  * Add 'has-thumbnail' class to posts with thumbnails
  */
-add_filter( 'post_class', 'custom_post_class' );
-function custom_post_class( $classes ) {
+add_filter( 'post_class', 'miseenplace_post_class' );
+function miseenplace_post_class( $classes ) {
 	global $post;
 	if ( has_post_thumbnail( $post->ID ) ) {
 		$classes[] = 'has-thumbnail';
@@ -205,13 +203,13 @@ function custom_post_class( $classes ) {
 /**
  * Excerpts
  */
-add_filter( 'excerpt_length', 'custom_excerpt_length', 10 );
-function custom_excerpt_length( $length ) {
+add_filter( 'excerpt_length', 'miseenplace_excerpt_length', 10 );
+function miseenplace_excerpt_length( $length ) {
 	return 40; // words
 }
 
-add_filter( 'excerpt_more', 'custom_excerpt_more' );
-function custom_excerpt_more( $more ) {
+add_filter( 'excerpt_more', 'miseenplace_excerpt_more' );
+function miseenplace_excerpt_more( $more ) {
 	global $post;
 	return '&hellip;';
 }
@@ -225,25 +223,4 @@ function add_archive_to_search( $classes ) {
 		$classes[] = 'archive';
 	}
 	return $classes;
-}
-
-/**
- * Brand the admin
- */
-add_action( 'login_head', 'project_admin_head' );
-add_action( 'admin_head', 'project_admin_head' );
-function project_admin_head() {
-	?><style type="text/css">
-		/* overwrite login logo */
-		.login h1 a {
-			background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/logo.png);
-			height: 45px;
-			opacity: 0.8;
-		}
-		#header-logo {
-			background-image: url(<?php echo get_stylesheet_directory_uri(); ?>/img/icon-32x32.gif);
-			background-size: 16px 16px;
-			opacity: 0.55;
-		}
-	</style><?php
 }
